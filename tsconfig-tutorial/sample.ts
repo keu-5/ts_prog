87bba9edd8d70331d30f15448b6be8ea8128ba76
keu-5 <keu02.78@gmail.com>
2024-02-05 15:42:25 +0900

    update

---
+++ tsconfig-tutorial/sample.ts
@@ -0,0 +1,8 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person): string {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
